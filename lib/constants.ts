0e400ecbfc04c2d07515235e0ed3f2e8b0cfbe3d
Sahaj Jain <sahajjain13@gmail.com>
2025-10-01 16:58:50 +0530

    increase max ai free requests to 5

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const AI_PROMPT_CHARACTER_LIMIT = 500;
 
 export const DEBOUNCE_DELAY = 50;
 
-export const AI_REQUEST_FREE_TIER_LIMIT = 3;
+export const AI_REQUEST_FREE_TIER_LIMIT = 5;
 
 export const MAX_IMAGE_FILES = 2;
 export const MAX_IMAGE_FILE_SIZE = 4 * 1024 * 1024; // 4MB
